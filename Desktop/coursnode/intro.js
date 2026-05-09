a529c5e00ccbde08c8266df2fff7fcc1b4ef4992
FitahiaAime <hei.fitahia@gmail.com>
2026-05-10 01:59:34 +0300

    coursnode

---
+++ Desktop/coursnode/intro.js
@@ -0,0 +1,29 @@
+// let nom = "blabla"
+// console.log(nom);
+
+// const nom = "nomm";
+// console.log(nom);
+
+// function bonjour(nom) {
+//     console.log("Salut toi " + nom);
+    
+// }
+
+// bonjour("bffbfbfb");
+// bonjour("b222222ffbfbfb");
+
+// const tab = ["fbvbvb, fdfdfd, ssdfdfdfdfd"];
+// console.log(tab);
+// tab.push
+// console.log(tab[3]);
+
+// setTimeout(() => {
+//     console.log("bonjour");
+//     clearInterval(temps);
+    
+// }, 11000);
+
+// var temps = setInterval(() => {
+//     console.log('nyrhrhf');
+
+// }, 10000);
