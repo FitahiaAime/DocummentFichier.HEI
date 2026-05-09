de0a680ecc5732e505ee99309114bd81f72781f2
FitahiaAime <hei.fitahia@gmail.com>
2026-05-10 01:07:14 +0300

    Ajout fichiers HTML CSS JS

---
+++ age.js/js.js
@@ -0,0 +1,22 @@
+// function salut(nom) {
+//     console.log("salut  toi :" + (nom));
+    
+// }
+// salut("bkkbkbkk");
+// salut("guguguyuygu");
+
+const etudiant = [
+    {
+        nom : "yvette",
+        niveau : 1
+    },
+
+    {
+        nom : "yvette jkkbkb",
+        niveau : 2
+    }
+];
+setTimeout(() => {
+   console.log(etudiant); 
+}, 5000);
+
